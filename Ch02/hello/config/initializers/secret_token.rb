4ac0563f032a2e9e6c3e43dce8c4ba38b14525c0
Apress <gwenanspearing@apress.com>
2016-10-17 18:44:54 +0100

    First commit

---
+++ Ch02/hello/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hello::Application.config.secret_key_base = 'b432b585507f667fc6218fbf673409c017e6df22fdebe7fe04bfc663ad3457d6f6fdde92afd7f3b282fd8f67e5fd021110f476c8b5f68ada226434ecbaca69af'
---
+++ Ch09/blog/config/routes.rb
@@ -0,0 +1,10 @@
+Blog::Application.routes.draw do
+  root :to => "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  resources :users
+  resource :session
+  get '/login' => "sessions#new", :as => "login"
+  get '/logout' => "sessions#destroy", :as => "logout"
+end
